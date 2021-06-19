586ac68729fbf1a4b4f4cb258309e6c8fbb9ae85
PickyDogFish <pifko.kosir@gmail.com>
2021-06-19 14:46:27 +0200

    still just going through the tutorial, part 9

---
+++ lib/cogs/fun.py
@@ -0,0 +1,11 @@
+from discord.ext.commands import Cog
+
+class Fun(Cog):
+    def __init__(self, bot):
+        self.bot=bot
+    @Cog.listener()
+    async def on_ready(self):
+        print("cog ready?")
+
+def setup(bot):
+    bot.add_cog(Fun(bot))
