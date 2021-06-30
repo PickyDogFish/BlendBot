2c5288a49f4ae97716dc1ee608cf1ec0f28f87e9
PickyDogFish <pifko.kosir@gmail.com>
2021-06-30 18:58:32 +0200

    counts number of votes and edits the message when a new person votes

--- lib/cogs/reactions.py
+++ lib/cogs/reactions.py
@@ -1,6 +1,7 @@
 from lib.bot import VOTING_CHANNEL_ID
 from discord.ext.commands import Cog
 from ..db import db
+from discord import Embed
 
 switcher = {"1️⃣": 1, "2️⃣": 2, "3️⃣": 3, "4️⃣": 4, "5️⃣": 5}
 
@@ -29,7 +30,21 @@ class Reactions(Cog):
             #check if emoji was from 1 to 5 and if voting is still active for this submission
             if emoji != None and db.field("SELECT previousChallengeID FROM currentChallenge WHERE challengeTypeID = 0") == db.field("SELECT challengeID FROM submission WHERE votingMsgID = ?", payload.message_id):
                 print("vote added to DB")
+                #check if new vote or a change of vote
+                newVote = True
+                if db.field("SELECT * FROM votes WHERE votingMsgID = ? AND voterID = ?", payload.message_id, payload.user_id) != None:
+                    newVote = False
+                
                 db.execute("REPLACE INTO votes (votingMsgID, voterID, vote) VALUES (?, ?, ?)",payload.message_id, payload.user_id, emoji)
+                #updating the number of votes
+                if newVote:
+                    oldMessage = await self.bot.get_channel(VOTING_CHANNEL_ID).fetch_message(payload.message_id)
+                    oldEmbed = oldMessage.embeds[0]
+                    numOfVotes = db.field("SELECT COUNT(voterID) FROM votes WHERE votingMsgID = ?", payload.message_id)
+                    embeded = Embed(title="Has collected " + str(numOfVotes) + " votes", colour = 0x5965F2)
+                    embeded.set_author(name=oldEmbed.author.name, icon_url=oldEmbed.author.icon_url)
+                    embeded.set_image(url=oldEmbed.image.url)
+                    await oldMessage.edit(embed = embeded)
             channel = self.bot.get_channel(payload.channel_id)
             user = self.bot.get_user(payload.user_id)
             msg = await channel.fetch_message(payload.message_id)
