4f4a31fb8490f6fbf4f58a1417e478811b194790
PickyDogFish <pifko.kosir@gmail.com>
2022-01-28 19:32:24 +0100

    added adminhelp command

--- lib/cogs/adminCommands.py
+++ lib/cogs/adminCommands.py
@@ -8,6 +8,7 @@ from ..db import db
 from datetime import date, datetime, timedelta
 import json
 from lib.bot import LOG_CHANNEL_ID, OWNER_IDS, SUBMIT_CHANNEL_ID
+from discord import Embed
 
 class Admin(Cog):
     def __init__(self, bot):
@@ -138,21 +139,24 @@ class Admin(Cog):
     #                 db.execute("INSERT OR IGNORE INTO themes (themeName, themeStatus) VALUES (?,0)", line.strip().replace("_", " "))
 
 
-    #sends a message of max 100 suggested themes
+    #sends a message of max 50 suggested themes
     @command(name="showsuggestions", aliases=["suggestions", "sugg"])
     async def show_suggestions(self, ctx):
         if ctx.author.guild_permissions.administrator:
-            listOfSuggestions = db.column("SELECT themeName FROM themes WHERE themeStatus = 0 LIMIT 100")
+            listOfSuggestions = db.column("SELECT themeName FROM themes WHERE themeStatus = 0 LIMIT 50")
             await ctx.send(listOfSuggestions)
 
-    #sends a message of max 100 rejected themes
+    #sends a message of all rejected themes
     @command(name="showrejected")
     async def show_rejected(self, ctx):
         if ctx.author.guild_permissions.administrator:
-            listOfRejected = db.column("SELECT themeName FROM themes WHERE themeStatus = -1 LIMIT 100")
-            await ctx.send(listOfRejected)
+            listOfRejected = db.column("SELECT themeName FROM themes WHERE themeStatus = -1")
+            for i in range(50, len(listOfRejected), 50):
+                await ctx.send(listOfRejected[i-50:i])
+                if i+50 > len(listOfRejected):
+                    await ctx.send(listOfRejected[i:])
 
-    #sends a message of max 100 approved themes
+    #sends a list of all approved themes
     @command(name="showapproved")
     async def show_rejected(self, ctx):
         if ctx.author.guild_permissions.administrator:
@@ -162,6 +166,7 @@ class Admin(Cog):
                 if i+50 > len(listOfApproved):
                     await ctx.send(listOfApproved[i:])
 
+    #sends a list of 50 least recently used themes (these are in the pool to be chosen from for the daily challenge)
     @command(name="showthemes", aliases=["themes"])
     async def show_themes(self,ctx):
         if ctx.author.guild_permissions.administrator:
@@ -205,17 +210,46 @@ class Admin(Cog):
             voters = db.records("SELECT voterID, count(votingMsgID) as numOfVotes FROM votes GROUP BY voterID ORDER BY numOfVotes DESC")
             names = ""
             for id, count in voters:
-                names += self.bot.get_user(id).display_name +": " + str(count) + ", "
+                try:
+                    names += self.bot.get_user(id).display_name +": " + str(count) + ", \n"
+                except:
+                    pass
             await ctx.send(names)
 
-    @command(name="setisinserver")
-    async def set_isinserver(self,ctx):
-        if ctx.author.id in OWNER_IDS:
-            users = db.records("SELECT userID FROM users")
-            for user in users:
-                userObject = self.bot.get_user(user[0])
-                if userObject == None:
-                    db.execute("UPDATE users SET isInServer = 0 WHERE userID = ?", user[0])
+    # @command(name="setisinserver")
+    # async def set_isinserver(self,ctx):
+    #     if ctx.author.id in OWNER_IDS:
+    #         users = db.records("SELECT userID FROM users")
+    #         for user in users:
+    #             userObject = self.bot.get_user(user[0])
+    #             if userObject == None:
+    #                 db.execute("UPDATE users SET isInServer = 0 WHERE userID = ?", user[0])
+
+    @command(name="adminhelp")
+    async def show_admin_help(self, ctx):
+        if ctx.author.guild_permissions.administrator:
+            helpText = "List of commands, with the prefix **$**:\n\n\n"
+            helpText += "`$clear [number]`: Deletes last *number* messages.\n\n"
+            helpText += "`$addusertodb [userId]`: Adds user entry into db with userId. \n\n"
+            helpText += "`$reject [theme_name]`: Sets the status of a theme to rejected.\n\n"
+            helpText += "`$approve [theme_name]`: Sets the status of a theme to approved.\n\n"
+            helpText += "`$setnotused [theme_name]`: sets last used date to 2011-11-11 11:11:11.\n\n"
+            helpText += "`$setused [theme_name]`: sets last used to today.\n\n"
+            helpText += "`$setdaily [theme_name]`: sets the current daily to theme_name.\n\n"
+            helpText += "`$givexp [userID] [xpAmount]`: gives user with userID xpAmount of renderXP. xpAmount can be negative.\n\n"
+            helpText += "`$showsuggestions`: shows suggestions without status. also `$sugg` or `$suggestions`.\n\n"
+            helpText += "`$showapproved`: shows all approved themes.\n\n"
+            helpText += "`$showrejected`: shows all rejected themes.\n\n"
+            helpText += "`$showthemes`: shows 50 themes that are in the pool for the next daily.\n\n"
+            helpText += "`$makelb`: remakes the leaderboard.\n\n"
+            helpText += "`$dodaily`: runs the daily challenge function.\n\n"
+            helpText += "`$showvoters`: shows a list of voters.\n\n"
+            helpText += "`$setcustom [name] [link] [numOfDays] [numOfVotingDays]`: set things for a custom challenge.\n\n"
+            helpText += "`$docustom`: runs the custom challenge function.\n\n"
+
+
+            embeded = Embed(title="Admin help for 3Daily bot.", colour = 16754726, description = helpText)
+            await ctx.send(embed = embeded)
 
 def setup(bot):
     bot.add_cog(Admin(bot))
